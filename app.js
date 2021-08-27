37f8ea681ad645b874145e520443aa98f1715886
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-08-27 15:50:34 -0700

    added button and spans to story

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // import functions
+
+// reference needed DOM elements
 const dir1 = document.getElementById('dir-1');
 const dir1Input = document.getElementById('dir-1-input');
 
@@ -9,14 +11,19 @@ const noun1 = document.getElementById('noun-1');
 const noun1Input = document.getElementById('noun-1-input');
 
 const number1 = document.getElementById('number-1');
+const number2 = document.getElementById('number-2');
 const number1Input = document.getElementById('number-1-input');
 
+const revealButton = document.getElementById('reveal-button');
 
-
-// reference needed DOM elements
-
-
-// set event listeners 
+// set event listeners
+revealButton.addEventListener('click', () => {
+    dir1.textContent = dir1Input.value;
+    dir2.textContent = dir2Input.value;
+    noun1.textContent = noun1Input.value;
+    number1.textContent = number1Input.value;
+    number2.textContent = number1Input.value;
+});
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
