b054fc350fcaa16b380872f43de1ed635898ef0a
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-08-27 15:01:14 -0700

    added DOM elements and story block

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 // import functions
+const dir1 = document.getElementById('dir-1');
+const dir1Input = document.getElementById('dir-1-input');
+
+const dir2 = document.getElementById('dir-2');
+const dir2Input = document.getElementById('dir-2-input');
+
+const noun1 = document.getElementById('noun-1');
+const noun1Input = document.getElementById('noun-1-input');
+
+const number1 = document.getElementById('number-1');
+const number1Input = document.getElementById('number-1-input');
+
+
 
 // reference needed DOM elements
 
